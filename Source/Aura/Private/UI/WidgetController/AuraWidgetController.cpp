213d4624c22bdaf901e556fec805176ad2e3cf80
MJL1106 <matthew.lowe2468@gmail.com>
2024-07-28 12:51:10 +0100

    Created overlay widget controller
    Linked the player controller, player state, ASC, and attribute set to the overlay widget

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,3 +3,15 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+FWidgetControllerParams::FWidgetControllerParams()
+	: PlayerController(nullptr), PlayerState(nullptr), AbilitySystemComponent(nullptr), AttributeSet(nullptr)
+{
+}
+
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams)
+{
+	PlayerController = WCParams.PlayerController;
+	PlayerState = WCParams.PlayerState;
+	AbilitySystemComponent = WCParams.AbilitySystemComponent;
+	AttributeSet = WCParams.AttributeSet;
+}
