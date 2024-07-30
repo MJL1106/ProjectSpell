6aa15a8c4ce9a2e7a7565430bec363d88e975432
MJL1106 <matthew.lowe2468@gmail.com>
2024-07-30 12:58:58 +0100

    Created gameplay effect delegates
    Broadcasts set up for gameplay tags on gameplay effects

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -5,11 +5,14 @@
 
 void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
-	
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec,
                                                 FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+
+	EffectAssetTags.Broadcast(TagContainer);
 }
