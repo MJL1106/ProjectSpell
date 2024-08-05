40ca9aa3f37c90d093cd96f48641805253ef268a
MJL1106 <matthew.lowe2468@gmail.com>
2024-08-05 15:54:25 +0100

    Added new tags to map to input actions
    New tags created for each input action to be used for gameplay abilities. Used a data asset to store the different Input actions and their Tag

--- Source/Aura/Private/Input/AuraInputConfig.cpp
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -3,3 +3,19 @@
 
 #include "Input/AuraInputConfig.h"
 
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FAuraInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag == InputTag)
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Ability Input Action for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	return nullptr;
+}
