7d7af67847feeaf3f000c94aeb1b66166ca77c8d
MJL1106 <matthew.lowe2468@gmail.com>
2024-08-21 12:26:07 +0100

    Created a function to get actors within a radius
    Function is used to get actors in a radius when the attack anim notify is fired off to the gameplay ability

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -26,10 +26,17 @@ public:
 	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
 
 	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
-	virtual void Die() override;
+
 	
 	UFUNCTION(NetMulticast, Reliable)
 	virtual void MulticastHandleDeath();
+
+	/** Combat Interface */
+	virtual void Die() override;
+	virtual FVector GetCombatSocketLocation_Implementation() override;
+	virtual bool IsDead_Implementation() const override;
+	virtual AActor* GetAvatar_Implementation() override;
+	/** End Combat Interface */
 	
 protected:
 
@@ -41,8 +48,8 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	FName WeaponTipSocketName;
 
-	virtual FVector GetCombatSocketLocation() override;
-
+	bool bDead = false;
+	
 	UPROPERTY()
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
