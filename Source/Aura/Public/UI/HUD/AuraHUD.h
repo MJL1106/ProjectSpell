6fb44fce610be3989f220190b9f6c08f7b53e9f6
MJL1106 <matthew.lowe2468@gmail.com>
2024-09-13 18:44:45 +0200

    Refactored AbilitySystemLibrary and constructed the spell menu widget controller in blueprint

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -12,6 +12,8 @@ class UAbilitySystemComponent;
 struct FWidgetControllerParams;
 class UOverlayWidgetController;
 class UAuraUserWidget;
+class USpellMenuWidgetController;
+
 /**
  * 
  */
@@ -22,8 +24,8 @@ class AURA_API AAuraHUD : public AHUD
 public:
 	
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
-	
 	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
+	USpellMenuWidgetController* GetSpellMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -46,4 +48,10 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
+
+	UPROPERTY()
+	TObjectPtr<USpellMenuWidgetController> SpellMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<USpellMenuWidgetController> SpellMenuWidgetControllerClass;
 };
