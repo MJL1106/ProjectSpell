de078a85f9d462a1c6ab62045aed9db5b900e191
MJL1106 <matthew.lowe2468@gmail.com>
2024-09-15 21:11:38 +0200

    Setup Spell menu buttons to enable or disable depending on the status of the Ability in the menu

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -194,7 +194,12 @@ FString("Resistance to Physical damage")
 FString("Hit React")
 	);
 
-	/* Abilties */
+	/* Abilities */
+
+	GameplayTags.Abilities_None = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Abilities.None"),
+FString("No Ability - like the nullptr for Ability Tags")
+	);
 	
 	GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Attack"),
