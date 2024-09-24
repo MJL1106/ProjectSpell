6c21ec4ffd9b262245b8d45205c57dda508e8f78
MJL1106 <matthew.lowe2468@gmail.com>
2024-09-24 21:08:56 +0100

    Created a function to get closest actors to impacted actor
    Function uses an algorithm to get the closest actors to the impacted actor so that the electrocute ability will be able to spread to other targets when it gets levelled up

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -105,6 +105,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|Gameplaymechanics")
 	static void GetLivePlayersWithinRadius(const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, const TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
 
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|GameplayEffects")
+	static void GetClosestTargets(int32 MaxTargets, const TArray<AActor*>& Actors, TArray<AActor*>& OutClosestTargets, const FVector& Origin);
+
 	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|Gameplaymechanics")
 	static bool IsNotFriend(AActor* FirstActor, AActor* SecondActor);
 
