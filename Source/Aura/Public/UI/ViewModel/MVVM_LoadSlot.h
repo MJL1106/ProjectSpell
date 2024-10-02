05e105f144b43f4ca2decfda58356d7bb4d4750c
MJL1106 <matthew.lowe2468@gmail.com>
2024-10-02 15:18:00 +0100

    Constructed initial view model controllers
    Added view model controllers for each load screen widget and set their slot index on the widget switcher.

---
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MVVMViewModelBase.h"
+#include "MVVM_LoadSlot.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FSetWidgetSwitcherIndex, int32, WidgetSwitcherIndex);
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMVVM_LoadSlot : public UMVVMViewModelBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FSetWidgetSwitcherIndex SetWidgetSwitcherIndex;
+
+	void InitializeSlot();
+
+	UPROPERTY()
+	FString PlayerName;
+
+	UPROPERTY()
+	FString LoadSlotName;
+
+	UPROPERTY()
+	FString SlotIndex;
+};
