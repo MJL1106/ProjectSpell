33bc415837fbc99e01a49a1dc55f34818eacffbf
MJL1106 <matthew.lowe2468@gmail.com>
2024-10-09 15:12:20 +0100

    Added actor to spawn enemies
    Created a spawn volume actor and spawn points actor. The spawn points can be placed in the world then added to the array on the spawn volume so when it gets overlapped enemies spawn in.

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -36,6 +36,11 @@ UMVVM_LoadSlot* UMVVM_LoadScreen::GetLoadSlotViewModelByIndex(int32 Index) const
 void UMVVM_LoadScreen::NewSlotButtonPressed(int32 Slot, const FString& EnteredName)
 {
 	AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(this));
+	if (!IsValid(AuraGameMode))
+	{
+		GEngine->AddOnScreenDebugMessage(1, 15.f, FColor::Magenta, FString("Please switch to single player"));
+		return;
+	}
 
 	LoadSlots[Slot]->SetMapName(AuraGameMode->DefaultMapName);
 	LoadSlots[Slot]->SetPlayerName(EnteredName);
@@ -103,6 +108,7 @@ void UMVVM_LoadScreen::PlayButtonPressed()
 void UMVVM_LoadScreen::LoadData()
 {
 	AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(this));
+	if (!IsValid(AuraGameMode)) return;
 	
 	for (const TTuple<int32, UMVVM_LoadSlot*> LoadSlot : LoadSlots)
 	{
