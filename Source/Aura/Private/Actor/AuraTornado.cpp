a69c74dd12f915317f4e582ffb6ff31d5397edaa
MJL1106 <matthew.lowe2468@gmail.com>
2024-10-15 21:59:16 +0200

    Implemented damage for tornado
    Tornado now deals damage on impact and every 1 second after impact. Added the damage curve for the tornado.

--- Source/Aura/Private/Actor/AuraTornado.cpp
+++ Source/Aura/Private/Actor/AuraTornado.cpp
@@ -3,17 +3,37 @@
 
 #include "Actor/AuraTornado.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "NiagaraFunctionLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Interaction/EnemyInterface.h"
 #include "Kismet/GameplayStatics.h"
 
 AAuraTornado::AAuraTornado()
 {
+	PrimaryActorTick.bCanEverTick = true;
+	
 	USceneComponent* Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
 	SetRootComponent(Root);
 
 	Sphere->SetupAttachment(Root);
+}
+
+void AAuraTornado::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	for (AActor* AffectedActor : AffectedActors)
+	{
+		if (IsValid(AffectedActor))
+		{
+			ApplyBobbingEffectToActor(AffectedActor, DeltaSeconds);
+		}
+	}
 }
 
 void AAuraTornado::OnHit()
@@ -32,3 +52,106 @@ void AAuraTornado::OnHit()
 	}
 	bHit = true;
 }
+
+void AAuraTornado::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(!IsValidOverlap(OtherActor)) return;
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor);
+	
+	if (!bHit && !TargetASC)
+	{
+		OnHit();
+	}
+	
+	if (HasAuthority())
+	{
+		if (TargetASC)
+		{
+			if (!AffectedActors.Contains(OtherActor))
+			{
+				ApplyDamageToActor(OtherActor, TargetASC);
+
+				// Stop tornado moving when hits actor
+				ProjectileMovement->Velocity = FVector::ZeroVector;
+
+				if (UCharacterMovementComponent* CharacterMovement = OtherActor->FindComponentByClass<UCharacterMovementComponent>())
+				{
+					CharacterMovement->DisableMovement();
+				}
+				
+				AffectedActors.Add(OtherActor);
+				InitialLocations.Add(OtherActor, OtherActor->GetActorLocation());
+				RunningTimeMap.Add(OtherActor, 0.0f);
+
+				// Applies Damage every second
+				GetWorldTimerManager().SetTimer(DamageTimerHandle, FTimerDelegate::CreateUObject(this, &AAuraTornado::CauseRepeatingDamage, OtherActor, TargetASC), 1.0f, true);
+
+				// Destroys the tornado after 3 seconds
+				GetWorldTimerManager().SetTimer(TornadoDestroyTimerHandle, this, &AAuraTornado::PrepareDestroyTornado, 3.0f, false);
+			}
+		}
+		else
+		{
+			Destroy();
+		}
+	}
+	else
+	{
+		bHit = true;
+	}
+}
+
+void AAuraTornado::ApplyDamageToActor(AActor* ActorToDamage, UAbilitySystemComponent* TargetASC)
+{
+	if (!ActorToDamage) return;
+	
+	const FVector DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+	DamageEffectParams.DeathImpulse = DeathImpulse;
+
+	DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
+	UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+}
+
+void AAuraTornado::CauseRepeatingDamage(AActor* ActorToDamage, UAbilitySystemComponent* TargetASC)
+{
+	ApplyDamageToActor(ActorToDamage, TargetASC);
+}
+
+void AAuraTornado::ApplyBobbingEffectToActor(AActor* AffectedActor, float DeltaTime)
+{
+	if (!AffectedActor) return;
+
+	FVector InitialLocation = InitialLocations[AffectedActor];
+	float& RunningTime = RunningTimeMap[AffectedActor];
+
+	RunningTime += DeltaTime;
+
+	const float Sine = SineAmplitude * (FMath::Sin(RunningTime * SinePeriodConstant) + 1.0f);
+	FVector CalculatedLocation = InitialLocation + FVector(0.f, 0.f, Sine);
+
+	AffectedActor->SetActorLocation(CalculatedLocation);
+}
+
+void AAuraTornado::PrepareDestroyTornado()
+{
+	for (AActor* AffectedActor : AffectedActors)
+	{
+		if (IsValid(AffectedActor))
+		{
+			if (UCharacterMovementComponent* CharacterMovement = AffectedActor->FindComponentByClass<UCharacterMovementComponent>())
+			{
+				CharacterMovement->SetMovementMode(EMovementMode::MOVE_Walking);
+			}
+		}
+	}
+
+	AffectedActors.Empty();
+	InitialLocations.Empty();
+	RunningTimeMap.Empty();
+
+	OnHit();
+	Destroy();
+}
+
+
--- Source/Aura/Public/Actor/AuraTornado.h
+++ Source/Aura/Public/Actor/AuraTornado.h
@@ -16,9 +16,47 @@ class AURA_API AAuraTornado : public AAuraProjectile
 
 public:
 	AAuraTornado();
+	
+	virtual void Tick(float DeltaSeconds) override;
+	
 
 protected:
 
 	virtual void OnHit() override;
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	
+	void ApplyBobbingEffectToActor(AActor* AffectedActor, float DeltaTime);
+	void ApplyDamageToActor(AActor* ActorToDamage, UAbilitySystemComponent* TargetASC);
+	void CauseRepeatingDamage(AActor* ActorToDamage, UAbilitySystemComponent* TargetASC);
+	void PrepareDestroyTornado();
+
+	UPROPERTY()
+	TArray<AActor*> AffectedActors;
+
+	UPROPERTY()
+	TSet<AActor*> DamagedActors;
+
+	UPROPERTY()
+	AActor* CurrentAffectedActor = nullptr;
+
+	UPROPERTY()
+	TMap<AActor*, FVector> InitialLocations;
+
+	UPROPERTY()
+	TMap<AActor*, float> RunningTimeMap;
+
+	UPROPERTY()
+	FTimerHandle TornadoDestroyTimerHandle;
+
+	UPROPERTY()
+	FTimerHandle DamageTimerHandle;
+
+	UPROPERTY()
+	FTimerHandle BobbingTimerHandle;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float SineAmplitude = 50.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float SinePeriodConstant = 2.0f;
 };
