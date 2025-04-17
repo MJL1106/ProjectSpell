e5cd35faf079bf7e798378563e8214020a919e03
MJL1106 <matthew.lowe2468@gmail.com>
2025-04-17 12:31:41 +0100

    Commented code

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -11,19 +11,25 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
 	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	// Calculate the angle between each minion (total spread divided by number of minions)
 	const float DeltaSpread = SpawnSpread / NumMinions;
 
+	// Calculate the leftmost direction of the spawn spread
 	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnSpread/2.f, FVector::UpVector);
 	TArray<FVector> SpawnLocations;
 	for (int32 i = 0; i < NumMinions; i++)
 	{
 		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread*i, FVector::UpVector);
+		
+		// Choose a random distance between the min and max spawn distance. Then multiply by the direction to get the actual position vector
 		FVector ChosenSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
 
+		// Checks if there's ground surface to place the minion on
 		FHitResult Hit;
 		GetWorld()->LineTraceSingleByChannel(Hit, ChosenSpawnLocation + FVector(0.f, 0.f, 400.f), ChosenSpawnLocation - FVector(0.f, 0.f, 400.f), ECC_Visibility);
 		if (Hit.bBlockingHit)
 		{
+			// Adjusts the spawn location to be the exact point that the line trace hits the surface
 			ChosenSpawnLocation = Hit.ImpactPoint;
 		}
 		SpawnLocations.Add(ChosenSpawnLocation);
